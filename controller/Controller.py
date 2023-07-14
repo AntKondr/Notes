0a43e58f0d6ca4ed67096a62c8a808603bc868d3
Anton_Kondratev <elkondro@gmail.com>
2023-07-14 13:26:35 +0700

    created ServiceID

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,4 +1,3 @@
-from time import time
 from model.Model import Model
 from views.consoleUI.ConsoleUI import ConsoleUI
 
@@ -11,6 +10,4 @@ class Controller:
         self.view = view
 
     def start_app(self):
-        self.view.show_hello()
-        note = self.model.create_note_obj(1, time(), "head here", "text content")
-        self.view.show_note(note)
+        self.view.wellcome()
--- model/Note.py
+++ model/Note.py
@@ -1,14 +1,30 @@
 class Note:
+    @staticmethod
+    def from_dict(dict_note: dict):
+        return Note(id=dict_note["id"],
+                    created_timestamp=dict_note["created_timestamp"],
+                    head=dict_note["head"],
+                    text=dict_note["text"],
+                    last_change_timestamp=dict_note["last_change_timestamp"])
+
     def __init__(self,
                  id: int,
-                 created_timestamp: int,
+                 created_timestamp: float,
                  head: str,
-                 text: str):
+                 text: str,
+                 last_change_timestamp: float = None):
         self.__id: int = id
-        self.__created_timestamp: int = created_timestamp
+        self.__created_timestamp: int = int(created_timestamp)
         self.__head: str = head
         self.__text: str = text
-        self.__last_change_timestamp = None
+        self.__last_change_timestamp: int = int(last_change_timestamp)
+
+    def __dict__(self) -> dict:
+        return {"id": self.__id,
+                "created_timestamp": self.__created_timestamp,
+                "head": self.__head,
+                "text": self.__text,
+                "last_change_timestamp": self.__last_change_timestamp}
 
     def get_id(self) -> int:
         return self.__id
@@ -28,9 +44,9 @@ class Note:
     def edit(self,
              place: str,
              new_content: str,
-             change_timestamp: int) -> None:
+             change_timestamp: float) -> None:
         if place == "1":
             self.__head = new_content
         elif place == "2":
             self.__text = new_content
-        self.__last_change_timestamp = change_timestamp
+        self.__last_change_timestamp = int(change_timestamp)
