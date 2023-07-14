f9bb765d0f2e6353d0966e054953aa03fa5e9c7d
Anton_Kondratev <elkondro@gmail.com>
2023-07-14 14:39:57 +0700

    module Model done (except edit_note func)

--- model/Model.py
+++ model/Model.py
@@ -16,49 +16,66 @@ class Model:
             pass
 
         self.__serviceID = ServiceID()
-        self.__cache: list[Note] = []
+        self.__cache: dict[Note] = {}
 
         try:
             with open(self.__DATAFILE_PATH, "r") as file:
-                dict_notes: list[dict] = load(file)
-            for d in dict_notes:
-                self.__cache.append(Note.from_dict(d))
+                dict_notes: dict[dict] = load(file)
+            for id in dict_notes:
+                self.__cache[id] = Note.from_dict(dict_notes[id])
         except FileNotFoundError:
             with open(self.__DATAFILE_PATH, "w") as file:
-                file.write("[]")
+                file.write("{}")
 
     def add_new_note(self,
                      head: str,
-                     text: str) -> bool:
+                     text: str) -> int:
         try:
             id = self.__serviceID.get_new_id()
             created_timestamp = time()
             note = Note(id, created_timestamp, head, text)
-            self.__cache.append(note)
+            self.__cache[id] = note
             self.save_cache()
-            return True
-        except:
-            return False
+            return 0
+        except FileNotFoundError:
+            return 1
 
-    def get_all_notes(self) -> list[Note]:
+    def get_all_notes(self) -> dict[Note]:
         return self.__cache
 
-    def find_note(self, find: str) -> list[Note]:
-        for note in self.__cache:
-            pass
+    def find_note(self, find_text: str) -> list[Note]:
+        result: list[Note] = []
+        for id in self.__cache:
+            note = self.__cache[id]
+            note_content = f"{note.get_head()} {note.get_text()}"
+            if find_text in note_content:
+                result.append(note)
+        return result
 
     def edit_note_by_id(id: str, what: str, change: str) -> str:
         pass
 
-    def del_note_by_id(id: str) -> bool:
-        pass
+    def get_note_by_id(self, id: int) -> Note:
+        try:
+            return self.__cache[id]
+        except KeyError:
+            return None
 
-    def get_note_by_id(id: str) -> Note:
-        pass
+    def del_note_by_id(self, id: int) -> int:
+        try:
+            del self.__cache[id]
+            self.save_cache()
+            return 0
+        except KeyError:
+            return 1
 
-    def save_cache(self) -> None:
-        dict_notes = []
-        for note in self.__cache:
-            dict_notes.append(note.__dict__())
-        with open(self.__DATAFILE_PATH, "w", encoding="utf-8") as file:
-            dump(dict_notes, file)
+    def save_cache(self) -> int:
+        try:
+            dict_notes = {}
+            for id in self.__cache:
+                dict_notes[id] = self.__cache[id].__dict__()
+            with open(self.__DATAFILE_PATH, "w", encoding="utf-8") as file:
+                dump(dict_notes, file)
+            return 0
+        except IsADirectoryError:
+            return 1
